d500d35eb67b908c4b40e2d7fb1cd6e0c79222f2
Sanoj <panditsanoj124@gmail.com>
2025-09-08 13:25:03 +0530

    understanding rename statement and to keyword

---
+++ rename_statement_and_to_keyword.sql
@@ -0,0 +1,9 @@
+use temp_db;
+
+-- to show all the tables
+show tables;
+
+-- to rename table name from a to b
+rename table employee to emp;
+
+rename table emp to abc; -- if the table with the name (after to) already exist then it will give error
