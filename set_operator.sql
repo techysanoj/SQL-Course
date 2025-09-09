1680dfd77ed84cbad0e8cc63fcb71c73e62e02a9
Sanoj <panditsanoj124@gmail.com>
2025-09-09 12:38:24 +0530

    understanding set operator use case

---
+++ set_operator.sql
@@ -0,0 +1,16 @@
+-- different kind of set operator are present
+-- purpose:
+/*
+	1. when we want to get the data from two tables
+	2. when we have divided a single table into multiple one and then we want to merge both the table data
+*/
+
+-- multiple set operators are
+/*
+	1. Union
+    2. Union All
+    3. Intersect
+    4. Minus
+    
+    understand it by getting the vein graph in mathematics
+*/
